7dddef3e3ca77185cb8a2d993e8a20ce5d2221f4
mkfreeman <michael.k.freeman@gmail.com>
2020-04-22 16:23:45 -0400

    Add controls, close #2

---
+++ StationaryVoronoiAndPerlin/controls.js
@@ -0,0 +1,52 @@
+/* Functions for building controls: makeControls() gets called in sketch.js */
+// Settings for the controls we'll build
+const controlSettings = [
+    {
+        label: "Minimum Radius", id: "minRadius", minVal: 2, maxVal: 200, value: 100, step: 1, update: () => { }
+    },
+    {
+        label: "Maximum Radius", id: "maxRadius", minVal: 100, maxVal: 500, value: 300, step: 1, update: () => { }
+    },
+    {
+        label: "Radius Devisions", id: "radiusDivisions", minVal: 100, maxVal: 4000, value: 2000, step: 1, update: () => {
+            clear();
+        }
+    },
+    {
+        label: "Perlin Variablility", id: "perlinVariability", minVal: 1, maxVal: 100, value: 25, step: 1, update: () => {
+            clear();
+        }
+    },
+    {
+        label: "Number of Cells", id: "numberOfCells", minVal: 1, maxVal: 400, value: 100, step: 1, update: () => {
+            resetCells();
+        }
+    }
+];
+
+
+// Function to make a slider
+function makeSlider(options = { label: "Label", minVal: 0, maxVal: 50, value: 10, step: 1, parent: createDiv(), update: () => { } }) {
+    let wrapper = createDiv(options.label);
+    wrapper.parent(options.parent);
+    wrapper.class("slider");
+    let slider = createSlider(options.minVal, options.maxVal, options.value, options.step);
+    slider.input(options.update); // function to do on update
+    slider.class("form-control-range")
+    slider.parent(wrapper);
+    return (slider);
+}
+
+// Function to make all of the controls (e.g., sliders)
+function makeControls() {
+    let controlWrapper = createDiv().id("control-wrapper");
+    let controlHeader = createDiv("<h2>Controls</h2>");
+    controlHeader.parent(controlWrapper);
+    return controlSettings.map(d => {
+        d.parent = controlWrapper;
+        return {
+            id: d.id,
+            control: makeSlider(d)
+        }
+    })
+}
--- StationaryVoronoiAndPerlin/sketch.js
+++ StationaryVoronoiAndPerlin/sketch.js
@@ -1,20 +1,32 @@
-let minRadius = 100;
-let maxRadius = 300;
-let radiusDivisions = 2000;
-let perlinVariability = 25;
-var absoluteFrame = 0;
-var numberOfCells = 100;
 var cells = [];
+let controls = []; // define this globally
 
-function randomG(v, range){
+function randomG(v, range) {
     // approximates gaussian distribution by averaging repeated random numbers
     // v : number of repeats
     // range : maximum number
     var r = 0;
-    for(var i = 0; i < v; i++){
+    for (var i = 0; i < v; i++) {
         r += random(range);
     }
     return r / v;
+}
+
+function resetCells() {
+    // Get new number of cells
+    let numCells = controls.filter(d => d.id == "numberOfCells")[0].control.value();
+
+    voronoiClearSites()
+    cells = [];
+    // create center points of cells
+
+    for (var cell = 0; cell < numCells; cell++) {
+        cells.push([int(randomG(5, width)), int(randomG(5, height))]);
+    }
+
+    // pass center points to voronoi
+    voronoiSites(cells);
+    voronoi(700, 600, true);
 }
 
 function setup() {
@@ -26,34 +38,39 @@ function setup() {
     voronoiCellStroke(255);
     voronoiSiteStroke(255);
     voronoiSiteFlag(false);
-    // create center points of cells
-    for (var cell=0; cell<numberOfCells; cell++) {
-        cells.push([int(randomG(5, width)), int(randomG(5, height))]);
-    }
-    // pass center points to voronoi
-    voronoiSites(cells);
-    voronoi(700, 600, true);
+
+    // Make controls
+    controls = makeControls()
+
+    // Reset the cell values
+    resetCells()
+
+
 }
 
 function draw() {
+    // Get values from controls:
+    let vals = {};
+    controls.map(d => vals[d.id] = d.control.value());
     // draws for each frame
     background(255);
-    stroke(255,255,255);
+    stroke(255, 255, 255);
     fill('rgba(100,100,100,1)');
     // perlin noise loop for outer shape
     beginShape();
-    for (let a = 0; a < TWO_PI; a += TWO_PI/radiusDivisions) {
+    for (let a = 0; a < TWO_PI; a += TWO_PI / vals.radiusDivisions) {
         // loops through angles of circle
-        let xoff = map(cos(a), -1, 1, 0, minRadius/perlinVariability);
-        let yoff = map(sin(a), -1, 1, 0, minRadius/perlinVariability);
+        let xoff = map(cos(a), -1, 1, 0, vals.minRadius / vals.perlinVariability);
+        let yoff = map(sin(a), -1, 1, 0, vals.minRadius / vals.perlinVariability);
         // map radius to between min and max radii
-        let radius = map(noise(xoff, yoff), 0, 1, minRadius, maxRadius);
+        let radius = map(noise(xoff, yoff), 0, 1, vals.minRadius, vals.maxRadius);
         let x = radius * cos(a);
         let y = radius * sin(a);
         // adds new vertex to the circle at mapped radius
-        vertex(x + width/2, y + height/2)
+        vertex(x + width / 2, y + height / 2)
     }
     endShape(CLOSE);
     // draw voronoi
     voronoiDraw(0, 0, false, true);
 }
+
